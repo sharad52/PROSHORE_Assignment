a700c8393201f2d502442d576627cb4d3a82ab0e
Sharad Bhandari <chemist.sharad@gmail.com>
2023-03-19 14:24:35 +0545

    feat(content): db setup

--- PROSHORE/settings.py
+++ PROSHORE/settings.py
@@ -89,10 +89,22 @@ WSGI_APPLICATION = 'PROSHORE.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+#POSTGRESQL DB SETUP
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'proshore',
+        'USER': 'proshore',
+        'PASSWORD': 'proshore',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
