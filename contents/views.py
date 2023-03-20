67360cc1e541f371c7797135780e5516431e11c6
Sharad Bhandari <chemist.sharad@gmail.com>
2023-03-20 13:31:50 +0545

    db backup and pagination

--- contents/views.py
+++ contents/views.py
@@ -3,6 +3,7 @@ from django.views import View as DFView
 from contents.df_views import GlobalDFView
 from contents.models import Content
 from contents.serializers import ContentSerializer
+from contents.mixins import GlobalPaginatorMixin
 
 
 class ContentView(GlobalDFView):
@@ -11,12 +12,36 @@ class ContentView(GlobalDFView):
     serializer = ContentSerializer
 
 
-class ContentListView(DFView):
+class ContentListView(DFView, GlobalPaginatorMixin):
+    model = Content
+
+    def get_queryset(self):
+        qset = self.model.objects.all()
+        return qset
+    
+    def process_form(self, request):
+        """
+        update self.kwargs_dict as required
+        :param request: returns a list after processing the query form
+        :return: full list of matched items
+        """
+        full_list = None
+        if request.GET and 'query' in request.GET:
+            form = self.form_class(request.GET)
+            if form.is_valid():
+                pass
+        if full_list is None:
+            full_list = self.get_queryset()
+
+        return full_list
+    
+    def get_list(self, request):
+        return self.process_form(request)
     
     def get(self, request):
-        list_content = Content.objects.all()
+        lst_content = self.get_paginated_list(request)
         context = {
-            'list_content': list_content
+            'lst_content': lst_content
         }
         return render(request, 'content_list.html', context)
     
